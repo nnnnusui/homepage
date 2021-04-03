d4b1c1d94b552fb3aa31b37fd6e2945a35eac94e
nnnnusui <nnnnusui@gmail.com>
2021-04-04 03:16:51 +0900

    add: services myPage link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,27 @@
 import React from "react";
 import { NextPage } from "next";
+import Link from "../components/Link";
 
 const Home: NextPage = () => {
   return (
-    <>
-      <h1>Header1</h1>
-      <section>
-        <h1>Sub</h1>
+    <section className="h-full w-full flex flex-col justify-center text-center">
+      <h1 style={{ fontSize: "500%" }}>N4U</h1>
+      <p>nnnnusui.ga</p>
+      <section className="flex justify-center">
+        {/* <h1 className="text-center">Links</h1> */}
+        <Link service="twitter" url="https://twitter.com/nnnnusui_public" />
+        <Link service="github" url="https://github.com/nnnnusui" />
+        <Link
+          service="soundcloud"
+          url="https://soundcloud.com/next_for_your_ideal"
+        />
+        <Link service="niconico" url="https://www.nicovideo.jp/user/9425987" />
+        <Link
+          service="youtube"
+          url="https://www.youtube.com/channel/UCbKHgc19qt13_kA95c9Ht3A"
+        />
       </section>
-    </>
+    </section>
   );
 };
 

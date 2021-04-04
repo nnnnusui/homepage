f38706d78b42c6ab8f8fec353ecfff82365078ea
nnnnusui <nnnnusui@gmail.com>
2021-04-04 10:17:15 +0900

    add: ogp info in _document.tsx

---
+++ src/pages/_document.tsx
@@ -0,0 +1,42 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import React from "react";
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from "next/document";
+
+export default class extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    return await Document.getInitialProps(ctx);
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# website: http://ogp.me/ns/website#">
+          <title>home - nnnnusui.ga</title>
+          <meta name="twitter:card" content="summary" />
+          <meta name="twitter:site" content="@nnnnusui" />
+          <meta name="twitter:creator" content="@nnnnusui" />
+          <meta property="fb:app_id" content="198922981704616" />
+
+          <meta property="og:url" content="https://nnnnusui.ga" />
+          <meta property="og:type" content="website" />
+          <meta property="og:description" content="In production..." />
+          <meta
+            property="og:image"
+            content="https://avatars.githubusercontent.com/u/2351241"
+          />
+          <meta property="og:site_name" content="nnnnusui.ga" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}

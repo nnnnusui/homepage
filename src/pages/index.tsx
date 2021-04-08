422aa1dc1945b8609f2114ccf8debb6b697bb1ae
nnnnusui <nnnnusui@gmail.com>
2021-04-08 11:25:12 +0900

    add: domains and style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,21 +7,38 @@ const Home: NextPage = () => {
   return (
     <section className="h-full w-full flex flex-col justify-center text-center">
       <Title />
-      <p>nnnnusui.ga</p>
-      <section className="flex justify-center">
-        {/* <h1 className="text-center">Links</h1> */}
-        <Link service="twitter" url="https://twitter.com/nnnnusui_public" />
-        <Link service="github" url="https://github.com/nnnnusui" />
-        <Link
-          service="soundcloud"
-          url="https://soundcloud.com/next_for_your_ideal"
-        />
-        <Link service="niconico" url="https://www.nicovideo.jp/user/9425987" />
-        <Link
-          service="youtube"
-          url="https://www.youtube.com/channel/UCbKHgc19qt13_kA95c9Ht3A"
-        />
-      </section>
+      <p className="url">nnnnusui.ga</p>
+      <ul className="domains">
+        <li>
+          <a href="/blog">blog</a>
+        </li>
+      </ul>
+      <ul className="links">
+        <li>
+          <Link service="twitter" url="https://twitter.com/nnnnusui_public" />
+        </li>
+        <li>
+          <Link service="github" url="https://github.com/nnnnusui" />
+        </li>
+        <li>
+          <Link
+            service="soundcloud"
+            url="https://soundcloud.com/next_for_your_ideal"
+          />
+        </li>
+        <li>
+          <Link
+            service="niconico"
+            url="https://www.nicovideo.jp/user/9425987"
+          />
+        </li>
+        <li>
+          <Link
+            service="youtube"
+            url="https://www.youtube.com/channel/UCbKHgc19qt13_kA95c9Ht3A"
+          />
+        </li>
+      </ul>
     </section>
   );
 };

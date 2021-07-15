cba941476f00731c29fe55bc164db8f7fc9f5c52
nnnnusui <nnnnusui@gmail.com>
2021-07-15 13:38:35 +0900

    old

---
+++ src/skillset.ts
@@ -0,0 +1,103 @@
+const skillsets = [
+  {
+    kind: "tool",
+    name: "Git",
+    proficiency: "introducing",
+    since: {
+      date: "2018/05/23",
+      on: "https://github.com/nnnnusui/helloGit/commit/528553339072bc66e4cb3bf236f854cb646008a7",
+    },
+  },
+  {
+    kind: "service",
+    name: "GitHub Actions",
+    proficiency: "introduced",
+    since: {
+      date: "2020/09/18",
+      on: "https://github.com/nnnnusui/tryActions/commits/main",
+    },
+  },
+
+  {
+    kind: "language",
+    name: "Java",
+    proficiency: "familiar",
+    since: {
+      date: "2017/05",
+    },
+  },
+  {
+    kind: "language",
+    name: "Kotlin",
+    proficiency: "introduced",
+    since: {
+      date: "2018/05/23",
+      on: "https://github.com/nnnnusui/JsonParser/commit/71fccdd9eab666eea0998c977bde200537f8eee3",
+    },
+  },
+  {
+    kind: "language",
+    name: "Scala",
+    proficiency: "a little used",
+    since: {
+      date: "2019/05/27",
+    },
+  },
+
+  {
+    kind: "tool",
+    name: "Gradle",
+    proficiency: "introducing",
+  },
+
+  {
+    kind: "language",
+    name: "TypeScript",
+    proficiency: "a little used",
+    since: {
+      date: "2021/01/17",
+      on: "https://github.com/nnnnusui/WebPianoRoll/commit/f32a73d97d76283c817a75aa1d194c1b6e18d589",
+    },
+  },
+  {
+    kind: "library",
+    name: "React Hooks",
+    proficiency: "introduced",
+    since: {
+      date: "2021/01/17",
+      on: "https://github.com/nnnnusui/WebPianoRoll/commit/f32a73d97d76283c817a75aa1d194c1b6e18d589",
+    },
+  },
+
+  {
+    kind: "language",
+    name: "scss",
+    proficiency: "introducing",
+  },
+  {
+    kind: "language",
+    name: "stylus",
+    proficiency: "introducing",
+  },
+
+  {
+    kind: "language",
+    name: "Rust",
+    proficiency: "introducing",
+    since: {
+      date: "2020/08/30",
+      on: "https://github.com/nnnnusui/WinAPI-on-Rust/commit/69565b1b4ecf695f6256a23dd5dba0724f10d02e",
+    },
+  },
+
+  {
+    kind: "language",
+    name: "commandLine",
+    proficiency: "introduced",
+  },
+  {
+    kind: "language",
+    name: "PowerShell",
+    proficiency: "introduced",
+  },
+];

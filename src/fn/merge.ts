9ad0be450ee36621f0f763c1d42579f95de52026
nnnnusui <nnnnusui@gmail.com>
2023-12-04 22:14:59 +0000

    merge() accepts functions (LIFO) &
    - divide `type MergeObject`

--- src/fn/merge.ts
+++ src/fn/merge.ts
@@ -1,8 +1,10 @@
 export const merge = <
-  Types extends Record<PropertyKey, unknown>[]
->(...objs: Types): Merge<Types> => {
+  Types extends unknown[]
+>(...types: Types): Merge<Types> => {
+  const functions = types.filter((it) => typeof it === "function");
+  const objs = types.filter((it) => typeof it !== "function") as Record<PropertyKey, unknown>[];
   const keys = objs.flatMap((obj) => Object.keys(obj));
-  return Object.fromEntries(
+  const obj = Object.fromEntries(
     keys.map((key) =>
       [
         key,
@@ -21,6 +23,10 @@ export const merge = <
     ),
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   ) as any;
+  return [
+    ...functions.reverse(),
+    obj,
+  ].reduce((sum, it) => Object.assign(sum, it));
 };
 
 export type Merge<Types> =
@@ -33,14 +39,19 @@ export type Merge<Types> =
 type MergeOnce<A, B> =
   B extends Record<PropertyKey, unknown>
     ? A extends Record<PropertyKey, unknown>
-      ? {
-        [Key in keyof B | keyof A]: MergeValue<
-        ValueOr<A, Key, undefined>,
-        ValueOr<B, Key, undefined>
-        >
-      }
+      ? MergeObject<A, B>
       : MergeValue<A, B>
     : MergeValue<A, B>;
+
+type MergeObject<
+  A extends Record<PropertyKey, unknown>,
+  B extends Record<PropertyKey, unknown>
+> = {
+  [Key in keyof B | keyof A]: MergeValue<
+  ValueOr<A, Key, undefined>,
+  ValueOr<B, Key, undefined>
+  >
+}
 
 type MergeValue<A, B> =
   [B] extends [(undefined | null | never)]

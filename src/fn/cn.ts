5c5cddaf16574c79b2ff1a2d72bdf33d90bcba32
nnnnusui <nnnnusui@gmail.com>
2026-06-12 23:36:18 +0000

    :package: add [clsx, tailwind-merge] & define `cn()`

---
+++ src/fn/cn.ts
@@ -0,0 +1,7 @@
+import { ClassValue , clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+/** @public */
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}

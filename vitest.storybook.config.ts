0e79bd13deda40e5046e708e6e67b7cffa0daa41
nnnnusui <nnnnusui@gmail.com>
2026-06-13 22:20:09 +0000

    :hammer_and_wrench: separate config for vitest

---
+++ vitest.storybook.config.ts
@@ -0,0 +1,23 @@
+import { storybookTest } from "@storybook/addon-vitest/vitest-plugin";
+import { playwright } from "@vitest/browser-playwright";
+import path from "node:path";
+import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  plugins: [
+    tsconfigPaths(),
+    // The plugin will run tests for the stories defined in your Storybook config
+    // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
+    storybookTest({ configDir: path.join(__dirname, ".storybook") }),
+  ],
+  test: {
+    name: "storybook",
+    browser: {
+      enabled: true,
+      headless: true,
+      provider: playwright({}),
+      instances: [{ browser: "chromium" }],
+    },
+  },
+});
